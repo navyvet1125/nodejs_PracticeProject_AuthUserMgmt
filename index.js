a1ec2490d4c4a12c2cd93fbb5c4cb54bce48e7d3
Evan Washington <enavy04@gmail.com>
2024-04-25 22:34:34 -0700

    updated use session  added port number to console log

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const authenticatedUser = (username,password)=>{
 
 const app = express();
 
-app.use(session({secret:"fingerpint"},resave=true,saveUninitialized=true));
+app.use(session({secret:"fingerpint", resave: true, saveUninitialized: true}));
 
 app.use(express.json());
 
@@ -92,4 +92,4 @@ const PORT =5000;
 
 app.use("/friends", routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
